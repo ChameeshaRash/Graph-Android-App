32f127f4ad169f80089530a6b217d8d0f304cef7
ChameeshaRash <chamrashani@gmail.com>
2023-05-09 17:51:42 +0530

    added 3D models

--- app/src/main/java/com/university/graph/MainActivity2.java
+++ app/src/main/java/com/university/graph/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.university.graph;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -21,11 +25,26 @@ public class MainActivity2 extends AppCompatActivity {
         TextView txtModeTitle=findViewById(R.id.modelTitle);
         TextView txtModeDes=findViewById(R.id.modelDescription);
         ImageView imgModelFull=findViewById(R.id.modelImage);
+        Button btnModelImage=findViewById(R.id.btnModelView);
 
         txtModeTitle.setText(modelTitle);
         txtModeDes.setText(modelDes);
         imgModelFull.setImageResource(imgModel);
 
+        btnModelImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = "https://firebasestorage.googleapis.com/v0/b/graph-54ff0.appspot.com%2Fcatenoid.glb?alt=media";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("googlear://scene-viewer/1?file=" + url));
+                intent.setPackage("com.google.android.googlequicksearchbox");
+                startActivity(intent);
+            }
+        });
 
     }
+
+
+
 }
